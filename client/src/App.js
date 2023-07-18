c3be03d4131d2d74ec4d323ee8696cd361aedb7b
Rahul Ohol <rahulohol01@gmail.com>
2023-07-18 22:27:00 +0530

    frontend setup created

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center" }}>react app</div>
+    </>
+  );
+}
+
+export default App;
