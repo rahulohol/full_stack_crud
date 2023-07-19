7871de48292a2cef1c7135aa9dd9c996055964c1
Rahul Ohol <rahulohol01@gmail.com>
2023-07-19 17:57:21 +0530

    adding form created

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { AppBar, Toolbar, styled } from "@mui/material";
+
+import { NavLink } from "react-router-dom";
+
+const Header = styled(AppBar)`
+  background: #111111;
+`;
+
+const Tabs = styled(NavLink)`
+  color: #ffffff;
+  margin-right: 20px;
+  text-decoration: none;
+  font-size: 20px;
+`;
+
+const Navbar = () => {
+  return (
+    <Header position="static">
+      <Toolbar>
+        {/* <Tabs to="./" exact>
+          Code for Interview
+        </Tabs> */}
+        <Tabs to="/" exact>
+          All Users
+        </Tabs>
+        <Tabs to="add" exact>
+          Add User
+        </Tabs>
+      </Toolbar>
+    </Header>
+  );
+};
+
+export default Navbar;
